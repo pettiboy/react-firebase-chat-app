344e8729098e8a37e26f7a59c5667a844cd21f83
Hussain Pettiwala <hussain.pettiwala@gmail.com>
2021-12-20 23:46:21 +0530

    ✨ NEW: connnect firebase

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.FirebaseApiKey,
+  authDomain: process.env.FirebaseAuthDomain,
+  projectId: process.env.FirebaseProjectId,
+  storageBucket: process.env.FirebaseStorageBucket,
+  messagingSenderId: process.env.FirebaseMessagingSenderId,
+  appId: process.env.FirebaseAppId,
+};
